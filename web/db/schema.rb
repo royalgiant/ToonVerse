705389470cb797e9ab6e14d23986c99636dc2ac1
Donald Lee <donaldlee50@gmail.com>
2022-08-09 17:13:01 -0400

    Add follower/following feature

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_202415) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_205719) do
+  create_table "follows", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "wallet_address"
     t.string "first_name"
