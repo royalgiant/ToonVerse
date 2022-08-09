3c7c45d350e031eb09735fb86c6b2c284382bfde
Donald Lee <donaldlee50@gmail.com>
2022-08-09 19:28:38 -0400

    Added publications & Chapters migrations

--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,12 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_205719) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_09_230327) do
+  create_table "chapters", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "publication_id"
+    t.string "title"
+    t.integer "chapter"
+    t.string "ipfs_nft_token_uri"
+    t.datetime "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["publication_id"], name: "index_chapters_on_publication_id"
+  end
+
   create_table "follows", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "follower_id"
     t.integer "followed_user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "publications", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title"
+    t.string "author"
+    t.string "day"
+    t.integer "recurrence"
+    t.integer "views"
+    t.integer "likes"
+    t.float "rating"
+    t.text "description"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_publications_on_user_id"
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -33,4 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_205719) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "chapters", "publications"
+  add_foreign_key "publications", "users"
 end
