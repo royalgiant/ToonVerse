5c7d4afa7c68dbf12fe072877c123a9f9b778f28
Donald Lee <donaldlee50@gmail.com>
2022-08-09 16:41:15 -0400

    Added Rails app with User object

---
+++ web/db/migrate/20220809202415_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :wallet_address
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :address1
+      t.string :address2
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :country
+
+      t.timestamps
+    end
+  end
+end
