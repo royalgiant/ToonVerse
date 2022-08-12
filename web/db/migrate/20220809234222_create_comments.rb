91aba776b99cf8754426bfaf608248bdcba1f1e0
Donald Lee <donaldlee50@gmail.com>
2022-08-12 18:01:52 -0400

    * Added migrations and gems for comments, and activestorage.

--- web/db/migrate/20220809234222_create_comments.rb
+++ web/db/migrate/20220809234222_create_comments.rb
@@ -3,6 +3,10 @@ class CreateComments < ActiveRecord::Migration[7.0]
     create_table :comments do |t|
       t.belongs_to :chapter, foreign_key: true
       t.belongs_to :user, foreign_key: true
+      t.text :comment, null: false
+      t.integer :report
+      t.integer :likes
+      t.integer :dislikes
 
       t.timestamps
     end
--- web/db/schema.rb
+++ web/db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_09_230327) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_12_220045) do
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "chapters", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "publication_id"
     t.string "title"
@@ -20,6 +48,19 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_230327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["publication_id"], name: "index_chapters_on_publication_id"
+  end
+
+  create_table "comments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "chapter_id"
+    t.bigint "user_id"
+    t.text "comment", null: false
+    t.integer "report"
+    t.integer "likes"
+    t.integer "dislikes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["chapter_id"], name: "index_comments_on_chapter_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "follows", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -60,6 +101,10 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_09_230327) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chapters", "publications"
+  add_foreign_key "comments", "chapters"
+  add_foreign_key "comments", "users"
   add_foreign_key "publications", "users"
 end
