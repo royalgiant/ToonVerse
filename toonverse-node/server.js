382ee1a40ae0f824a18911dedcaf2eb1dad71bda
Donald Lee <donaldlee50@gmail.com>
2022-08-19 21:47:21 -0400

    Mint NFT through the server

--- toonverse-node/server.js
+++ toonverse-node/server.js
@@ -1,6 +1,7 @@
 import * as dotenv from 'dotenv'
 dotenv.config()
 import express from "express";
+import cors from "cors";
 const app = express()
 const port = 3001
 import AWS from "aws-sdk"
@@ -12,7 +13,7 @@ const ipfs = await IPFS.create()
 const fetch = await makeIpfsFetch({ipfs})
 const client = new NFTStorage({token: process.env.NFT_STORAGE_API_KEY})
 import Web3 from "web3"
-// const contract = require('truffle-contract');
+import ToonVerse from '../build/contracts/ToonVerse.json' assert {type: 'json'};
 // const artifacts = require('./build/Inbox.json');
 
 const s3 = new AWS.S3({
@@ -21,9 +22,18 @@ const s3 = new AWS.S3({
   Bucket: process.env.S3_BUCKET_DEV    
 });
 
+const network = process.env.ETHEREUM_NETWORK;
+if (typeof web3 !== 'undefined') {
+  var web3 = new Web3(web3.currentProvider); 
+} else {
+  var web3 = new Web3(new Web3.providers.HttpProvider(`https://${network}.infura.io/v3/${process.env.INFURA_API_KEY}`));
+}
+const networkId = await web3.eth.net.getId()
+const ToonVerseNetworkData = ToonVerse.networks[networkId]
+const contract = new web3.eth.Contract(ToonVerse.abi, ToonVerseNetworkData.address)
+
 // const contract = await ToonVerse.deployed()
 // await contract.mintNFT("0x9aB8d0C7B218a7971b6BFe580d9FDBe8E8adb98B", "ipfs://bafyreigqfy2gafrbxibnuj74isbh6fr6us2d224fril5dqquhniaqrij7y/metadata.json")
-
 async function storeImageNFT(id, image, content_type, name, description) {
   var image_blob = new Blob([image], {type: content_type})
   const nft = {
@@ -46,14 +56,34 @@ app.post('/upload_and_mint/:id/:name/:description', async (req,res)=>{
     var fileExtension = mime.extension(data.ContentType);
     var fileName = req.name+"."+fileExtension
     var metadata_url = await storeImageNFT(req.id, data.Body, data.ContentType, req.id+"_"+fileName, req.description)
+    console.log(process.env.TEST_ACCOUNT_PUBLIC_ADDESS)
+    // Generate a transaction to calls the `mintNFT` method
+    const tx = contract.methods.mintNFT(process.env.TEST_ACCOUNT_PUBLIC_ADDESS, metadata_url)
+    // Send the transaction to the network
+    const receipt = await tx
+    .send({
+      from: process.env.TEST_ACCOUNT_PUBLIC_ADDESS,// the account that'll pay for the transaction
+      gas: await tx.estimateGas(),
+    })
+    .on('transactionHash', (txhash) => {
+      console.log(`Mining transaction ...`)
+      console.log(`https://${network}.etherscan.io/tx/${txhash}`)
+    })
+    .on('error', function(error){
+      console.error(`An error happened: ${error}`)
+      callback()
+    })
+    .then(function(receipt){
+      // Success, you've minted the NFT. The transaction is now on chain!
+      console.log(
+          `Success: The NFT has been minted and mined in block ${receipt.blockNumber}`)
+      callback()
+    })
+
     res.json({metadata_url: metadata_url})
 })
 
-if (typeof web3 !== 'undefined') {
-    var web3 = new Web3(web3.currentProvider)
-  } else {
-    var web3 = new Web3(new Web3.providers.HttpProvider('https://localhost:8545'))
-}
+app.use(cors())
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
